cac427c2ea5d4e3d69373f81a34da07874eb8c7b
ToomTime Suparerk <sm.suparerk@gmail.com>
2025-09-22 16:08:52 +0700

    initial

---
+++ PowerWeb.ServiceDefaults/Configurations/AuthenConfigure.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerWeb.ServiceDefaults.Configurations
+{
+    public class AuthenConfigure
+    {
+        
+    }
+}
---
+++ PowerWeb.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PowerWeb.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
